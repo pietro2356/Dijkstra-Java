377dc6b72ff3f99dcbe485c8fa1e15412993494b
pietro2356 <pietro.rocchio@gmail.com>
2020-09-19 21:52:30 +0200

    Classi Dijkstra finite.

--- src/App.java
+++ src/App.java
@@ -1,7 +1,41 @@
+import java.util.HashSet;
+
 import dijkstra.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(final String[] args) throws Exception {
+        final Nodo nodeA = new Nodo("A");
+        final Nodo nodeB = new Nodo("B");
+        final Nodo nodeC = new Nodo("C");
+        final Nodo nodeD = new Nodo("D");
+        final Nodo nodeE = new Nodo("E");
+        final Nodo nodeF = new Nodo("F");
+
+        nodeA.addDestination(nodeB, 10);
+        nodeA.addDestination(nodeC, 15);
+
+        nodeB.addDestination(nodeD, 12);
+        nodeB.addDestination(nodeF, 15);
+
+        nodeC.addDestination(nodeE, 10);
+
+        nodeD.addDestination(nodeE, 2);
+        nodeD.addDestination(nodeF, 1);
+
+        nodeF.addDestination(nodeE, 5);
+
+        Graph graph = new Graph();
+        
+
+        graph.addNode(nodeA);
+        graph.addNode(nodeB);
+        graph.addNode(nodeC);
+        graph.addNode(nodeD);
+        graph.addNode(nodeE);
+        graph.addNode(nodeF);
+
+        graph = Dijkstra.calcPathGraph(graph, nodeA);
+
+        System.out.println(graph);
     }
 }
--- src/dijkstra/Dijkstra.java
+++ src/dijkstra/Dijkstra.java
@@ -1,12 +1,60 @@
 package dijkstra;
 
-import jdk.internal.jimage.ImageReader.Node;
+import java.util.Map.Entry;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
 
 public class Dijkstra {
-    public static Graph calcPercorso(Graph grafo, Node nodoSorgente) {
-        nodoSorgente.set
+
+    public static Graph calcPathGraph(Graph grafo, Nodo origine) {
         
+        origine.setDistance(0);
+        Set<Nodo> nodiEsaminati = new HashSet<>();
+        Set<Nodo> nodiDaEsaminare = new HashSet<>();
+
+        nodiDaEsaminare.add(origine);
+
+        while (nodiDaEsaminare.size() != 0) {
+            Nodo nodoCorrente = calcMinDistNodo(nodiDaEsaminare);
+            nodiDaEsaminare.remove(nodoCorrente);
+
+            for (Entry<Nodo, Integer> corrVicina : nodoCorrente.getAdjacentNodes().entrySet()) {
+                Nodo nodoVicino = corrVicina.getKey();
+                Integer pesoLink = corrVicina.getValue();
+
+                if (!nodiEsaminati.contains(nodoVicino)) {
+                    CalcDistMin(nodoVicino, pesoLink, nodoCorrente);
+                    nodiDaEsaminare.add(nodoVicino);
+                }
+            }
+            nodiEsaminati.add(nodoCorrente);
+        }
         
         return grafo;
+    }
+
+    private static void CalcDistMin(Nodo nodo, Integer peso, Nodo sorgente) {
+        Integer distanzaSorgente = sorgente.getDistance();
+
+        if (distanzaSorgente + peso < nodo.getDistance()) {
+            nodo.setDistance(distanzaSorgente + peso);
+            LinkedList<Nodo> percorsoMin = new LinkedList<>(sorgente.getShortestPath());
+            percorsoMin.add(sorgente);
+            nodo.setShortestPath(percorsoMin);
+        }
+    }
+
+    private static Nodo calcMinDistNodo(Set<Nodo> nodiDaEsaminare){
+        Nodo lowestDistanceNode = null;
+        int lowestDistance = Integer.MAX_VALUE;
+        for (Nodo node : nodiDaEsaminare) {
+            int nodeDistance = node.getDistance();
+            if (nodeDistance < lowestDistance) {
+                lowestDistance = nodeDistance;
+                lowestDistanceNode = node;
+            }
+        }
+        return lowestDistanceNode;
     }
 }
