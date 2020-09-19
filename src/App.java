53b5499ea6b58b805337d2d924136a219746de8b
pietro2356 <pietro.rocchio@gmail.com>
2020-09-19 22:29:45 +0200

    Non funge

--- src/App.java
+++ src/App.java
@@ -1,15 +1,19 @@
-import java.util.HashSet;
+import java.util.Arrays;
+import java.util.List;
 
-import dijkstra.*;
+import dijkstra.Dijkstra;
+import dijkstra.Graph;
+import dijkstra.Nodo;
 
 public class App {
+
     public static void main(final String[] args) throws Exception {
-        final Nodo nodeA = new Nodo("A");
-        final Nodo nodeB = new Nodo("B");
-        final Nodo nodeC = new Nodo("C");
-        final Nodo nodeD = new Nodo("D");
-        final Nodo nodeE = new Nodo("E");
-        final Nodo nodeF = new Nodo("F");
+        Nodo nodeA = new Nodo("A");
+        Nodo nodeB = new Nodo("B");
+        Nodo nodeC = new Nodo("C");
+        Nodo nodeD = new Nodo("D");
+        Nodo nodeE = new Nodo("E");
+        Nodo nodeF = new Nodo("F");
 
         nodeA.addDestination(nodeB, 10);
         nodeA.addDestination(nodeC, 15);
@@ -25,7 +29,6 @@ public class App {
         nodeF.addDestination(nodeE, 5);
 
         Graph graph = new Graph();
-        
 
         graph.addNode(nodeA);
         graph.addNode(nodeB);
@@ -36,6 +39,44 @@ public class App {
 
         graph = Dijkstra.calcPathGraph(graph, nodeA);
 
-        System.out.println(graph);
+        List<Nodo> shortestPathForNodeB = Arrays.asList(nodeA);
+        List<Nodo> shortestPathForNodeC = Arrays.asList(nodeA);
+        List<Nodo> shortestPathForNodeD = Arrays.asList(nodeA, nodeB);
+        List<Nodo> shortestPathForNodeE = Arrays.asList(nodeA, nodeB, nodeD);
+        List<Nodo> shortestPathForNodeF = Arrays.asList(nodeA, nodeB, nodeD);
+
+        for (Nodo node : graph.getNodes()) {
+            switch (node.getName()) {
+            case "B":
+                if (node.getShortestPath().equals(shortestPathForNodeB)) {
+                    System.out.println("B");
+                }
+                break;
+            case "C":
+                if (node.getShortestPath().equals(shortestPathForNodeC)) {
+                    System.out.println("C");
+                }
+                break;
+            case "D":
+                if (node.getShortestPath().equals(shortestPathForNodeD)) {
+                    System.out.println("D");
+                }
+                break;
+            case "E":
+                if (node.getShortestPath().equals(shortestPathForNodeE)) {
+                    System.out.println("E");
+                }
+                break;
+            case "F":
+                if (node.getShortestPath().equals(shortestPathForNodeF)) {
+                    System.out.println("F");
+                }
+                break;
+            default:
+                System.out.println(".");
+            break;
+            }
+        }
     }
+    
 }
