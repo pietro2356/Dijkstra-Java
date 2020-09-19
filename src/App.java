9fb6ec47f7ada484f9cb6009ccba2aad55bfe963
pietro2356 <pietro.rocchio@gmail.com>
2020-09-19 20:36:30 +0200

    sistemato il repo

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import ../lib.dijkstra.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
--- src/dijkstra/Graph.java
+++ src/dijkstra/Graph.java
@@ -3,10 +3,8 @@ package dijkstra;
 import java.util.HashSet;
 import java.util.Set;
 
-import jdk.internal.jimage.ImageReader.Node;
-
 public class Graph {
-    private Set<org.w3c.dom.Node> nodi = new HashSet<>();
+    private Set<Node> nodi = new HashSet<>();
 
     public void addNode(Node item) {
         nodi.add(item);
